f9ab4bd5e2cb1e01d62305760daae3163a224cda
DiegoAraaujo <diegoaraaujo8@gmail.com>
2026-06-02 14:53:30 -0300

    refactor(schema): merge user schemas into single file

--- src/domain/users/usersSchemas.ts
+++ src/domain/users/usersSchemas.ts
@@ -1,5 +1,13 @@
 import { z } from "zod";
 
+export const createUserSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters long").max(100),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(8, "Password must be at least 6 characters long"),
+});
+
+export type CreateUserBody = z.infer<typeof createUserSchema>;
+
 export const updateUserSchema = z
   .object({
     name: z
